9df095340d1603072ab0f80d82e0342660257e5e
Brian Rojas <marchinram@gmail.com>
2014-02-10 18:05:57 -0800

    Added fade transition

--- FlappyBird-UIKitDynamics/GameStartViewController.m
+++ FlappyBird-UIKitDynamics/GameStartViewController.m
@@ -8,7 +8,7 @@
 
 #import "GameStartViewController.h"
 
-@interface GameStartViewController ()
+@interface GameStartViewController () <UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning>
 
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 
@@ -28,6 +28,15 @@
     [super didReceiveMemoryWarning];
 }
 
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"GameBeginSegue"]) {
+        UIViewController *vc = segue.destinationViewController;
+        vc.transitioningDelegate = self;
+        vc.modalPresentationStyle = UIModalPresentationCustom;
+    }
+}
+
 #pragma mark - Private methods
 
 - (void)shakeTitleMessage
@@ -43,6 +52,50 @@
             self.titleLabel.frame = frame;
         } completion:^(BOOL finished) {
             [self shakeTitleMessage];
+        }];
+    }];
+}
+
+#pragma mark - UIViewControllerTransitioningDelegate methods
+
+- (id <UIViewControllerAnimatedTransitioning>)animationControllerForPresentedController:(UIViewController *)presented presentingController:(UIViewController *)presenting sourceController:(UIViewController *)source
+{
+    return self;
+}
+
+- (id <UIViewControllerAnimatedTransitioning>)animationControllerForDismissedController:(UIViewController *)dismissed
+{
+    return self;
+}
+
+#pragma mark - UIViewControllerAnimatedTransitioning methods
+
+- (NSTimeInterval)transitionDuration:(id <UIViewControllerContextTransitioning>)transitionContext
+{
+    return 0.8;
+}
+
+- (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext
+{
+    UIView *containerView = [transitionContext containerView];
+    
+    UIView *coverView = [[UIView alloc] initWithFrame:containerView.bounds];
+    coverView.backgroundColor = [UIColor blackColor];
+    coverView.alpha = 0.0f;
+    [containerView addSubview:coverView];
+    
+    UIView *toView = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey].view;
+    
+    [UIView animateWithDuration:0.4 animations:^{
+        coverView.alpha = 1.0f;
+    } completion:^(BOOL finished) {
+        [containerView addSubview:toView];
+        [containerView bringSubviewToFront:coverView];
+        [UIView animateWithDuration:0.4 animations:^{
+            coverView.alpha = 0.0f;
+        } completion:^(BOOL finished) {
+            [coverView removeFromSuperview];
+            [transitionContext completeTransition:YES];
         }];
     }];
 }
